b77fed6ef5095da37ad88c49cab7945d53b9ad4c
Ahmed Magdy <96287382+Ahmed-Magdy-Rizk@users.noreply.github.com>
2024-12-18 22:44:25 +0200

    Update Program.cs
    update Q11 on days from 30 to 31

--- Program.cs
+++ Program.cs
@@ -220,7 +220,7 @@ namespace Session03Assignment
             switch (MonthNumber)
             {
                 case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-                    days = 30; 
+                    days = 31; 
                     break;
                 case 4: case 6: case 9: case 11:
                     days = 30;
